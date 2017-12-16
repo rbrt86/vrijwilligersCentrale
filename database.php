cf896547afd9796c6b7bf5025fcfcdf613c663e2
Robert <meel4robert@hotmail.comm>
2017-12-16 22:46:51 +0100

    tryout db again

--- database.php
+++ database.php
@@ -8,14 +8,14 @@
 $dbhost = getenv('OPENSHIFT_MYSQL_DB_HOST') . ":" . getenv('OPENSHIFT_MYSQL_DB_PORT');
 $dbname = getenv('MYSQL_DATABASE');
 $username = getenv('MYSQL_USER');
-$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$password = getenv('MYSQL_PASSWD');
 
 echo "<br/>dbhost set to ".$dbhost."<br/>";
 echo "dbname set to ".$dbname."<br/>";
 echo "username set to ".$username."<br/>";
 
 
-		$conn = new mysqli($dbhost,$username,$password,$dbname);
+		$conn = new mysqli($username,$password,$dbname);
 
 		if ($conn->connect_error) {
 
