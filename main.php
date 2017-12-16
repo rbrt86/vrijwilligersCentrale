9ee18548e4a2dff7871c7b86dbb7677d078b8c4c
Robert <meel4robert@hotmail.comm>
2017-12-16 23:37:28 +0100

    counting page refreshes

--- main.php
+++ main.php
@@ -2,6 +2,7 @@
 include 'database.php';
 
 echo "<p/>page last refreshed: ".date("l h:i:s")."<p/>";
+echo "request is being processed by". gethostname();
 
 $clicks = "SELECT clicks FROM clickCounter";
 $result = $conn->query($clicks);
@@ -9,10 +10,21 @@ $result = $conn->query($clicks);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "Number of clicks so far: " . $row["clicks"]."<br/>";
+       $clickNumber = $row["clicks"];
+        echo "Number of clicks so far: " . $clickNumber."<br/>";
     }
 } else {
     echo "0 results";
 }
+
+$insert = "INSERT INTO clickCounter (clicks)
+VALUES ($clickNumber++)";
+
+if ($conn->query($insert) === TRUE) {
+  echo "<p/>Click succesvol";
+} else {
+  echo "<p/>Error: " . $sql . "<br>" . $conn->error;
+}
+
 $conn->close();
 ?>
