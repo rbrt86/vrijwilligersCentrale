25086b1c7c5fe8ce43ea198a797905ded8aac7dd
Robert <meel4robert@hotmail.comm>
2017-12-16 21:07:30 +0100

    fixed index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-<h1>Robert is pretty lousy developer</h1>
+echo '<html><body>';
+echo '<h1>Robert is pretty lousy developer</h1>';
 echo "<br/><a href='main.php'>just trying this link</a>";
+echo "</body></html>";
 ?>
