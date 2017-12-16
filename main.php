141e8ac38cf97427eb93f5d0d3302dd342e759bf
Robert <meel4robert@hotmail.comm>
2017-12-16 23:25:03 +0100

    reading number of clicks from db

--- main.php
+++ main.php
@@ -1,6 +1,18 @@
 <?php
-echo "trying to set up a database connection";
 include 'database.php';
 
+echo "<p/>page last refreshed: ".date("l h:i:s")."<p/>";
 
+$clicks = "SELECT clicks FROM clickCounter";
+$result = $conn->query($clicks);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Number of clicks so far: " . $row["clicks"]<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
