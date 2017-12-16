b20a237ab5af8e03a801fb3cb82505798b1fad58
Robert <meel4robert@hotmail.comm>
2017-12-16 23:03:47 +0100

    I suck at this..

--- database.php
+++ database.php
@@ -16,12 +16,12 @@ $password = getenv('MYSQL_PASSWD');
 		$conn = new mysqli($username,$password,$dbname);
 
 		if ($conn->connect_error) {
-
-			die("Connection failed, cannot connect to database: ". $conn->connection_error);
 			echo "<br/>";
 			echo "<br/>dbhost set to ".$dbhost."<br/>";
 			echo "dbname set to ".$dbname."<br/>";
 			echo "username set to ".$username."<br/>";
+			die("Connection failed, cannot connect to database: ". $conn->connection_error);
+
 
 		}
 
