a04ff4e5402abf5e504933c9d241455bbad430bd
Robert <meel4robert@hotmail.comm>
2017-12-18 12:32:46 +0100

    fixed typo in index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo '<html><body>';
-echo '<h1>Robert is pretty lousy developer</h1>';
+echo '<h1>Robert is a pretty lousy developer</h1>';
 echo "<br/><a href='main.php'>just trying this link</a>";
 echo "</body></html>";
 ?>
