0b698a0c491f8fff6568dfb83d9a2a26d605a744
Robert <meel4robert@hotmail.comm>
2017-12-18 21:05:53 +0100

    added pagecounter

--- main.php
+++ main.php
@@ -2,28 +2,28 @@
 include 'database.php';
 
 echo "<p/>page last refreshed: ".date("l h:i:s")."<p/>";
-echo "request is being processed by". gethostname();
+echo "request is being processed by ". gethostname()."<p/>";
 
-/*$clicks = "SELECT clicks FROM clickCounter";
+$clicks = "SELECT clicks FROM clickCounter where ID = 1";
 $result = $conn->query($clicks);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-       //$clickNumber = '$row["clicks"];'
-        echo "Number of clicks so far: " . $clickNumber."<br/>";
+       $clickNumber = $row["clicks"];
+        echo "Number of pageviews so far: " . $clickNumber."<br/>";
     }
 } else {
     echo "0 results";
 }
 
-$insert = "INSERT INTO clickCounter (clicks) VALUES ('1');
+$update = "UPDATE clickCounter SET clicks=".++$clickNumber." where ID=1";
 
-/*if ($conn->query($insert) === TRUE) {
-//  echo "<p/>Click succesvol";
-//} else {
-//  echo "<p/>Error: " . $sql . "<br/>" . $conn->error;
+if ($conn->query($update) === TRUE) {
+  echo "<p/>pageview updated";
+} else {
+  echo "<p/>Error: " . $sql . "<br/>" . $conn->error;
 }
-*/
+
 $conn->close();
 ?>
